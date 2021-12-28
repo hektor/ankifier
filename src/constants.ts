3ec77e0fd87b5aec97e17dff0a418b8430ecafff
Hektor Misplon <hektor.misplon@pm.me>
2021-12-28 16:50:46 +0100

    Lint & format

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,12 @@
-export const OBS_INLINE_MATH_REGEXP: RegExp = /(?<!\$)\$((?=[\S])(?=[^$])[\s\S]*?\S)\$/g
-export const OBS_DISPLAY_MATH_REGEXP: RegExp = /\$\$([\s\S]*?)\$\$/g
-export const OBS_CODE_REGEXP:RegExp = /(?<!`)`(?=[^`])[\s\S]*?`/g
-export const OBS_DISPLAY_CODE_REGEXP:RegExp = /```[\s\S]*?```/g
+export const OBS_INLINE_MATH_REGEXP = /(?<!\$)\$((?=[\S])(?=[^$])[\s\S]*?\S)\$/g
+export const OBS_DISPLAY_MATH_REGEXP = /\$\$([\s\S]*?)\$\$/g
+export const OBS_CODE_REGEXP = /(?<!`)`(?=[^`])[\s\S]*?`/g
+export const OBS_DISPLAY_CODE_REGEXP = /```[\s\S]*?```/g
 
 export const CODE_CSS_URL = `https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/arta.css`
 
 export function escapeRegex(str: string): string {
-    // Got from stackoverflow - https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
-    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  // Got from stackoverflow - https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
+  // eslint-disable-next-line no-useless-escape
+  return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 }
