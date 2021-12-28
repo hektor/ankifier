ba7d9b4e3e6c5d4c996798e5dcee41d2971d2c06
Hektor Misplon <hektor.misplon@pm.me>
2021-12-28 14:15:24 +0100

    Remove link from Obsidian settings

--- src/settings.ts
+++ src/settings.ts
@@ -398,7 +398,6 @@ export class SettingsTab extends PluginSettingTab {
 
 		containerEl.empty()
 		containerEl.createEl('h2', {text: 'Obsidian_to_Anki settings'})
-		containerEl.createEl('a', {text: 'For more information check the wiki', href: "https://github.com/Pseudonium/Obsidian_to_Anki/wiki"})
 		this.setup_note_table()
 		this.setup_folder_table()
 		this.setup_syntax()
