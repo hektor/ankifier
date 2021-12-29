8c1fbd846c0f58dd4c3b35ee24c4d7c71bcc1537
Hektor Misplon <hektor.misplon@pm.me>
2021-12-29 11:59:21 +0100

    Refactor 'anki.ts'

--- src/anki.ts
+++ src/anki.ts
@@ -1,5 +1,3 @@
-const ANKI_PORT = 8765
-
 import { AnkiConnectNote } from './interfaces/note-interface'
 
 export interface AnkiConnectRequest {
@@ -9,120 +7,88 @@ export interface AnkiConnectRequest {
 }
 
 export function invoke(action: string, params = {}) {
+  const PORT = 8765
+  const HOST = 'http://127.0.0.1'
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
-    xhr.addEventListener('error', () => reject('failed to issue request'))
+    xhr.addEventListener('error', () => reject('Failed to issue request'))
     xhr.addEventListener('load', () => {
       try {
         const response = JSON.parse(xhr.responseText)
-        if (Object.getOwnPropertyNames(response).length != 2) {
-          throw 'response has an unexpected number of fields'
-        }
-        if (!response.hasOwnProperty('error')) {
-          throw 'response is missing required error field'
-        }
-        if (!response.hasOwnProperty('result')) {
-          throw 'response is missing required result field'
-        }
-        if (response.error) {
-          throw response.error
-        }
+        if (Object.getOwnPropertyNames(response).length != 2)
+          throw 'Response has an unexpected number of fields'
+        if (!response.hasOwnProperty('error')) throw 'Response is missing required error field'
+        if (!response.hasOwnProperty('result')) throw 'Response is missing required result field'
+        if (response.error) throw response.error
         resolve(response.result)
       } catch (e) {
         reject(e)
       }
     })
 
-    xhr.open('POST', 'http://127.0.0.1:' + ANKI_PORT.toString())
+    xhr.open('POST', `${HOST}:${PORT}`)
     xhr.send(JSON.stringify({ action, version: 6, params }))
   })
 }
 
+/*
+/* Helper function for parsing the result of a multi
+ */
+
 export function parse<T>(response: { error: string; result: T }): T {
-  //Helper function for parsing the result of a multi
-  if (Object.getOwnPropertyNames(response).length != 2) {
+  if (Object.getOwnPropertyNames(response).length != 2)
     throw 'response has an unexpected number of fields'
-  }
-  if (!response.hasOwnProperty('error')) {
-    throw 'response is missing required error field'
-  }
-  if (!response.hasOwnProperty('result')) {
-    throw 'response is missing required result field'
-  }
-  if (response.error) {
-    throw response.error
-  }
+  if (!response.hasOwnProperty('error')) throw 'response is missing required error field'
+  if (!response.hasOwnProperty('result')) throw 'response is missing required result field'
+  if (response.error) throw response.error
   return response.result
 }
 
-// All the rest of these functions only return request objects as opposed to actually carrying out the action. For efficiency!
+/*
+ * Request wrapper
+ *
+ * Versions the request
+ * Set default for params
+ *
+ * Returns a request object
+ */
 
-function request(action: string, params = {}): AnkiConnectRequest {
-  return { action, version: 6, params }
-}
+const request = (action: string, params = {}): AnkiConnectRequest => ({
+  action,
+  version: 6,
+  params,
+})
 
-export function multi(actions: AnkiConnectRequest[]): AnkiConnectRequest {
-  return request('multi', { actions: actions })
-}
+/*
+ * Request functions returning various request objects
+ */
 
-export function addNote(note: AnkiConnectNote): AnkiConnectRequest {
-  return request('addNote', { note: note })
-}
+export const multi = (actions: AnkiConnectRequest[]): AnkiConnectRequest =>
+  request('multi', { actions })
 
-export function deleteNotes(note_ids: number[]): AnkiConnectRequest {
-  return request('deleteNotes', { notes: note_ids })
-}
+export const addNote = (note: AnkiConnectNote): AnkiConnectRequest => request('addNote', { note })
 
-export function updateNoteFields(id: number, fields: Record<string, string>): AnkiConnectRequest {
-  return request('updateNoteFields', {
-    note: {
-      id: id,
-      fields: fields,
-    },
-  })
-}
+export const deleteNotes = (notes: number[]): AnkiConnectRequest =>
+  request('deleteNotes', { notes })
 
-export function notesInfo(note_ids: number[]): AnkiConnectRequest {
-  return request('notesInfo', {
-    notes: note_ids,
-  })
-}
+export const updateNoteFields = (id: number, fields: Record<string, string>): AnkiConnectRequest =>
+  request('updateNoteFields', { note: { id, fields } })
 
-export function changeDeck(card_ids: number[], deck: string): AnkiConnectRequest {
-  return request('changeDeck', {
-    cards: card_ids,
-    deck: deck,
-  })
-}
+export const notesInfo = (notes: number[]): AnkiConnectRequest => request('notesInfo', { notes })
 
-export function removeTags(note_ids: number[], tags: string): AnkiConnectRequest {
-  return request('removeTags', {
-    notes: note_ids,
-    tags: tags,
-  })
-}
+export const changeDeck = (cards: number[], deck: string): AnkiConnectRequest =>
+  request('changeDeck', { cards, deck })
 
-export function addTags(note_ids: number[], tags: string): AnkiConnectRequest {
-  return request('addTags', {
-    notes: note_ids,
-    tags: tags,
-  })
-}
+export const removeTags = (notes: number[], tags: string): AnkiConnectRequest =>
+  request('removeTags', { notes, tags })
 
-export function getTags(): AnkiConnectRequest {
-  return request('getTags')
-}
+export const addTags = (notes: number[], tags: string): AnkiConnectRequest =>
+  request('addTags', { notes, tags })
 
-export function storeMediaFile(filename: string, data: string): AnkiConnectRequest {
-  return request('storeMediaFile', {
-    filename: filename,
-    data: data,
-  })
-}
+export const getTags = (): AnkiConnectRequest => request('getTags')
 
-export function storeMediaFileByPath(filename: string, path: string): AnkiConnectRequest {
-  return request('storeMediaFile', {
-    filename: filename,
-    path: path,
-  })
-}
+export const storeMediaFile = (filename: string, data: string): AnkiConnectRequest =>
+  request('storeMediaFile', { filename, data })
+
+export const storeMediaFileByPath = (filename: string, path: string): AnkiConnectRequest =>
+  request('storeMediaFile', { filename, path })
